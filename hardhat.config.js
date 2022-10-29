351aa63a00808a835fcaf991d7105551c552e63a
yoea13 <yoea13@gmail.com>
2022-10-29 16:34:38 -0300

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,21 @@
-require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("./tasks/block-number")
-require("@nomiclabs/hardhat-etherscan")
-require("dotenv").config()
-require("solidity-coverage")
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("./tasks/block-number");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("solidity-coverage");
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 const RINKEBY_RPC_URL =
   process.env.RINKEBY_RPC_URL ||
-  "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
-const PRIVATE_KEY =
-  process.env.PRIVATE_KEY ||
-  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+  "https://eth-rinkeby.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -44,4 +42,4 @@ module.exports = {
     noColors: true,
     coinmarketcap: COINMARKETCAP_API_KEY,
   },
-}
+};
